cb367461e57966899c4e3d6c83bcbfae82e441bf
YarKrav <jaroslav.krava@gmail.com>
2025-06-04 19:26:14 +0300

    Added ImageInsert method

---
+++ text_editor/ImageInsert.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace text_editor
+{
+    partial class ImageInsert
+    {
+        public static void InsertImage(RichTextBox richTextBox, string imagePath)
+        {
+            if (string.IsNullOrEmpty(imagePath) || !File.Exists(imagePath))
+            {
+                MessageBox.Show("File not found!");
+                return;
+            }
+
+            try
+            {
+                Image img = Image.FromFile(imagePath);
+
+                Clipboard.SetImage(img);
+
+                richTextBox.Paste();
+
+                img.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+    }
+}
+
