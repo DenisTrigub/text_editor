bdd18bb9137af81877d195f00775e383b54af51e
Antfenix <andreykravchenko14236@gmail.com>
2025-06-04 17:52:34 +0300

    Add metod Search

---
+++ text_editor/Search.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_search
+{
+    partial class Form1
+    {
+        private void textBoxSearch_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxSearch.Text))
+            {
+                int cursorPosition = richTextBox1.SelectionStart;
+
+                richTextBox1.SelectAll();
+                richTextBox1.SelectionBackColor = Color.White;
+
+                richTextBox1.Select(cursorPosition, 0);
+                richTextBox1.SelectionBackColor = Color.White;
+            }
+        }
+
+        private void buttonFind_Click(object sender, EventArgs e)
+        {
+            string searchText = textBoxSearch.Text;
+
+            if (string.IsNullOrWhiteSpace(searchText))
+            {
+                MessageBox.Show("Введіть текст для підсвічування.");
+                return;
+            }
+
+            int originalSelectionStart = richTextBox1.SelectionStart;
+            int originalSelectionLength = richTextBox1.SelectionLength;
+
+            // Очистка предыдущей подсветки
+            richTextBox1.SelectAll();
+            richTextBox1.SelectionBackColor = Color.White;
+
+            int startIndex = 0;
+            while (startIndex < richTextBox1.TextLength)
+            {
+                int index = richTextBox1.Find(searchText, startIndex, RichTextBoxFinds.None);
+                if (index == -1)
+                    break;
+
+                richTextBox1.Select(index, searchText.Length);
+                richTextBox1.SelectionBackColor = Color.Yellow;
+
+                startIndex = index + searchText.Length;
+            }
+
+            // Возвращаем курсор
+            richTextBox1.Select(originalSelectionStart, originalSelectionLength);
+            richTextBox1.ScrollToCaret();
+            richTextBox1.Focus();
+
+            // Сброс стиля после подсветки
+            int caretPosition = richTextBox1.SelectionStart + richTextBox1.SelectionLength;
+            richTextBox1.Select(caretPosition, 0);
+            richTextBox1.SelectionBackColor = Color.White;
+        }
+    }
+}
